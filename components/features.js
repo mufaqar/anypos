48acea1355167aa8b9901256050e23e906a48ab1
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2022-12-15 18:57:45 +0500

    icons

--- components/features.js
+++ components/features.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ImageBox from '../components/image_box';
-import OS_icon from '../public/images/1.png';
+import Icon1 from '../public/images/icon1.svg';
+import Icon2 from '../public/images/icon2.svg';
+import Icon3 from '../public/images/icon3.svg';
+import Icon4 from '../public/images/icon4.svg';
+import Icon5 from '../public/images/icon5.svg';
+import Icon6 from '../public/images/icon6.svg';
+import Icon7 from '../public/images/icon7.svg';
+import Icon8 from '../public/images/icon8.svg';
+import Icon9 from '../public/images/icon9.svg';
 import { useTranslation } from 'next-export-i18n';
 
 export default function Features() {
@@ -14,7 +22,7 @@ export default function Features() {
       </div>
       <div className="container mx-auto grid md:grid-cols-3 grid-cols-1 gap-10 ">
         <ImageBox
-          Featue_Img={OS_icon}
+          Featue_Img={Icon1}
           size="w-1/6"
           title="Operating System"
           features={[
@@ -22,7 +30,7 @@ export default function Features() {
           ]}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-2" />
           <ImageBox
-          Featue_Img={OS_icon}
+          Featue_Img={Icon2}
           size="w-1/6"
           title="Operating System"
           features={[
@@ -30,7 +38,7 @@ export default function Features() {
           ]}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-2" />
           <ImageBox
-          Featue_Img={OS_icon}
+          Featue_Img={Icon3}
           size="w-1/6"
           title="Operating System"
           features={[
@@ -38,7 +46,7 @@ export default function Features() {
           ]}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-2" />
           <ImageBox
-          Featue_Img={OS_icon}
+          Featue_Img={Icon4}
           size="w-1/6"
           title="Operating System"
           features={[
@@ -46,7 +54,7 @@ export default function Features() {
           ]}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-2" />
           <ImageBox
-          Featue_Img={OS_icon}
+          Featue_Img={Icon5}
           size="w-1/6"
           title="Operating System"
           features={[
@@ -54,7 +62,7 @@ export default function Features() {
           ]}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-2" />
           <ImageBox
-          Featue_Img={OS_icon}
+          Featue_Img={Icon6}
           size="w-1/6"
           title="Operating System"
           features={[
@@ -62,7 +70,7 @@ export default function Features() {
           ]}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-2" />
           <ImageBox
-          Featue_Img={OS_icon}
+          Featue_Img={Icon7}
           size="w-1/6"
           title="Operating System"
           features={[
@@ -70,7 +78,15 @@ export default function Features() {
           ]}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-2" />
           <ImageBox
-          Featue_Img={OS_icon}
+          Featue_Img={Icon8}
+          size="w-1/6"
+          title="Operating System"
+          features={[
+            'Android 8.1 WiseOS',
+          ]}
+          variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-2" />
+          <ImageBox
+          Featue_Img={Icon9}
           size="w-1/6"
           title="Operating System"
           features={[
