de48f6832883fb8af510decf64f1cc6d09bb93d7
Mufaqar Islam <mufaqar@gmail.com>
2022-12-17 21:26:21 +0500

    updated

--- components/features.js
+++ components/features.js
@@ -24,82 +24,65 @@ export default function Features() {
         <ImageBox
           Featue_Img={Icon1}
           size="w-1/6"
-          title="POS on any Device"
-          features={[
-            'anyPOS software works on any mobile, tab or PC. Get full POS without spending thousands on clunky hardware and legacy software’s.',
-          ]}
+          title={t('features1.heading')}
+          features={t('features1.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
         <ImageBox
           Featue_Img={Icon2}
           size="w-1/6"
-          title="POS on any Device"
-          features={[
-            'Print simplified invoices in compliance with ZATCA standards.',
-          ]}
+          title={t('features2.heading')}
+          features={t('features2.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
         <ImageBox
           Featue_Img={Icon3}
           size="w-1/6"
-          title="It’s on Cloud!"
-          features={[
-            "As anyPOS is on the cloud so you can access it anytime from anywhere in the word it's available 24/7. See the live progress of your store sales through our integrated dashboard. ",
-          ]}
+          title={t('features3.heading')}
+          features={t('features3.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
         <ImageBox
           Featue_Img={Icon4}
           size="w-1/6"
-          title="Record Payments"
-          features={[
-            'With anyPOS you can Record payment quickly and easily. We Support Mada Visa Master Etc',
-          ]}
+          title={t('features4.heading')}
+          features={t('features4.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
         <ImageBox
           Featue_Img={Icon5}
           size="w-1/6"
-          title="Online & Offline"
-          features={[
-            'anyPOS is a cloud-based platform that can also work offline. All offline transactions are automatically synced with your store’s cloud once you get back online. So, continue selling, even when there’s no internet.',
-          ]}
+          title={t('features5.heading')}
+          features={t('features5.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
         <ImageBox
           Featue_Img={Icon6}
           size="w-1/6"
-          title="Multi-Store Management"
-          features={[
-            'Manage multiple stores under one account from the dashboard. Track your sales, employees, and customer.',
-          ]}
+          title={t('features6.heading')}
+          features={t('features6.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
         <ImageBox
           Featue_Img={Icon7}
           size="w-1/6"
-          title="High Level Reports"
-          features={[
-            'anyPOS central dashboard gives you the ability to review sales, customer, and overall business performances. Make better decisions with anyPOS cloud-based POS.',
-          ]}
+          title={t('features7.heading')}
+          features={t('features7.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
+
         <ImageBox
           Featue_Img={Icon8}
           size="w-1/6"
-          title="Win XP"
-          features={[
-            'No need to change or upgrade your current POS  as anyPOS works on win XP. ',
-          ]}
+          title={t('features9.heading')}
+          features={t('features9.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
         <ImageBox
           Featue_Img={Icon9}
           size="w-1/6"
-          title="Multi-Language"
-          features={[
-            ' anyPOS supports multiple languages, and allows users to use multiple languages on any device.',
-          ]}
+          title={t('features9.heading')}
+          features={t('features9.sub_heading')}
           variations="bg-white py-5 rounded-[5px] hover:shadow-lg flex gap-3 px-3 items-start"
         />
       </div>
--- components/image_box.js
+++ components/image_box.js
@@ -1,23 +1,22 @@
 import Image from 'next/image';
 import React from 'react';
 
-
-export default function ImageBox({ title, features, Featue_Img, variations, size }) {
-    return (
-        <div className={`${variations}`}>
-            <Image src={Featue_Img} alt="Image" className={`${size}`} />
-            <div>
-                <h5 className='text-[#2A2A2A] text-xl font-bold tracking-[0.5px]'>
-                    {title}
-                </h5>
-                <ul>
-                    {features?.map((feature) => (
-                        <li key={feature} className='text-[#54595F] text-base font-medium'>
-                            {feature}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    )
+export default function ImageBox({
+  title,
+  features,
+  Featue_Img,
+  variations,
+  size,
+}) {
+  return (
+    <div className={`${variations}`}>
+      <Image src={Featue_Img} alt="Image" className={`${size}`} />
+      <div>
+        <h5 className="text-[#2A2A2A] text-xl font-bold tracking-[0.5px]">
+          {title}
+        </h5>
+        <p className="text-[#54595F] text-base font-medium">{features}</p>
+      </div>
+    </div>
+  );
 }
