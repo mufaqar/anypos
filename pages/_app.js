c7386daac0d7b13e421c4c27bdf3db171c98ed93
Mufaqar Islam <mufaqar@gmail.com>
2022-12-20 12:22:49 +0500

    udpated types

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import '../styles/globals.css';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import { useRouter } from 'next/router';
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const { query } = router;
+  // console.log('🚀 ~ file: _app.tsx:9 ~ App ~ router', query);
+
+  return (
+    <>
+      <div
+        dir={query === '' ? '' : query.lang === 'ar' ? 'rtl' : ''}
+        lang={query.lang === 'ar' ? 'ar' : 'en'}
+        className={query.lang === 'ar' && 'rtl'}
+      >
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
+  );
+}
